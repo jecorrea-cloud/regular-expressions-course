6396a6029e2e068725ba3e4a94a084817a083bd5
joaquin <jecorrea@protonmail.com>
2022-05-26 11:43:22 -0400

    added text

--- greedy-quantifiers.js
+++ greedy-quantifiers.js
@@ -1,8 +1,8 @@
-// Greedy quantifiers are used to find whatever matches anywhere.
+// Greedy quantifiers can be used to separate text.
 // .	Match with anything even if it is empty
 // g    Match anything globally.
 
-const regex = /<whiskey>.*?<\/whiskey>/;
+const regex = /<whiskey>.*?<\/whiskey>/g;
 const text = "<whiskey>Jack Daniels</whiskey><whiskey>Jameson</whiskey>";
 
 // This will return an array with two elements. If the `?` is taken off, it'll return a single one
